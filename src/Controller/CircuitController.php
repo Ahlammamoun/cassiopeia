8ba6f0a204c4d9504db11aa28bfc40a15ef7b9b7
Ahlammamoun <mestar.ale@gmail.com>
2023-10-11 21:24:22 +0200

    crud test on circuit entity

--- src/Controller/CircuitController.php
+++ src/Controller/CircuitController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CircuitRepository;
+use App\Entity\Circuit;
+use Doctrine\ORM\EntityManagerInterface;
+
+
 
 
 class CircuitController extends AbstractController
@@ -30,9 +34,9 @@ class CircuitController extends AbstractController
     /**
      * @Route("/", name="cursus")
      */
-    public function list(circuitRepository $circuitRepo): Response
+    public function list(circuitRepository $circuitRepository): Response
     {
-        $cursus = $circuitRepo->findAll();
+        $cursus = $circuitRepository->findAll();
         
         //return new Response("symfony");
 
@@ -42,6 +46,90 @@ class CircuitController extends AbstractController
             ]
          );
     }
+
+
+
+     /**
+     * @Route("/cursus/create", name="cursus_create")
+     */
+    public function create(EntityManagerInterface $doctrine)
+    {
+
+        $newCursus = new Circuit();
+        $newCursus->setTitle("MBA ping pong");
+        $newCursus->setDescription("MBA ping pong");
+        $newCursus->setDuration(4);
+        $newCursus->setDifficulties(5);
+        $newCursus->setSuccess(90);
+        $newCursus->setDiplomas(6);
+        $newCursus->setPicture('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzIxGr3fYeh-l173RsqkMkOrClQVDmxYirxA&usqp=CAU');
+
+
+
+        //dump($newCursus);
+
+        $doctrine->persist($newCursus);
+
+        $doctrine->flush();
+
+        //dd($newCursus);
+
+        return $this->redirectToRoute("cursus");
+
+    }
+
+      /**
+     * @Route("/cursus/update/{id}", requirements={"id": "\d+"})
+     */
+    public function update($id, CircuitRepository $circuitRep, EntityManagerInterface $doctrine)
+    {
+
+        $cursus = $circuitRep->find($id);
+
+        $cursus->setTitle('Ahlam ' . mt_rand(2, 99));
+
+
+        $doctrine->flush();
+
+
+        return $this->redirectToRoute("circuit", array("id" => $id));
+
+
+    }
+    
+
+    /**
+     * @Route("/cursus/delete/{id}", requirements={"id": "\d+"})
+     */
+    public function delete($id, CircuitRepository $circuitRep, EntityManagerInterface $doctrine)
+    {
+
+        $cursus = $circuitRep->find($id);
+
+        $doctrine->remove($cursus);
+    
+        $doctrine->flush();
+
+
+        return $this->redirectToRoute("cursus");
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
