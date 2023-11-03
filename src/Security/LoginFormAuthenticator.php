958000e117d076b8ff0092b1b0e4f36b12ba66b1
Ahlammamoun <mestar.ale@gmail.com>
2023-11-03 20:06:33 +0100

    display button deconnection if user connected

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -50,7 +50,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('cursus'));
+        return new RedirectResponse($this->urlGenerator->generate('app_back_circuit_index'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
